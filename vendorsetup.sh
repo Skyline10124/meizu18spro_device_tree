193d801852c360c582547d33c523ed192f0a4ae2
Skyline10124 <75871983+Skyline10124@users.noreply.github.com>
2023-12-14 20:32:48 +0800

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for meizu MEIZU 18s Pro (meizu18sPro)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_meizu18sPro-user
+add_lunch_combo omni_meizu18sPro-userdebug
+add_lunch_combo omni_meizu18sPro-eng
